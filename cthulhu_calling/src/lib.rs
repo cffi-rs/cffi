c83ed24540b82f36f700f39a88eec04688ddd8bc
Pascal Hertleif <pascal@technocreatives.com>
2019-07-16 14:57:58 +0200

    Add more type conversions
    They worshipped, so they said, the Great Old Ones who lived ages before
    there were any men.

--- cthulhu_calling/src/lib.rs
+++ cthulhu_calling/src/lib.rs
@@ -3,6 +3,7 @@ use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
     punctuated::Punctuated,
+    spanned::Spanned,
     ArgCaptured, FnArg, Type,
 };
 
@@ -24,60 +25,110 @@ pub fn call_with(
 
     let syn::ItemFn { ident: ref name, ref decl, .. } = fn_item.clone();
     let syn::FnDecl { inputs: ref params, output: ref return_type, .. } = *decl.clone();
-    let c_params: Punctuated<FnArg, syn::Token![,]> = params.iter().flat_map(to_c_param).collect();
+
+    let mut c_params: Punctuated<FnArg, syn::Token![,]> = Punctuated::new();
+    for param in params {
+        c_params.extend(to_c_param(param)?);
+    }
 
     let rust_fn = &raw_function;
-    let body: syn::Block = syn::parse2(quote!{{
+    let body: syn::Block = syn::parse2(quote! {{
         #rust_fn
 
         unimplemented!()
-    }}).unwrap();
+    }})?;
 
     Ok(syn::ItemFn {
-        abi: Some(syn::parse2(quote!(extern "C")).unwrap()),
-        decl: Box::new(syn::FnDecl { inputs: c_params, ..*decl.clone() }),
+        abi: Some(syn::parse2(quote!(extern "C"))?),
+        decl: Box::new(syn::FnDecl {
+            inputs: c_params,
+            generics: syn::parse2(quote!())?,
+            ..*decl.clone()
+        }),
         block: Box::new(body),
         ..fn_item
-    }.into_token_stream())
+    }
+    .into_token_stream())
 }
 
-fn to_c_param(arg: &FnArg) -> Vec<FnArg> {
+fn to_c_param(arg: &FnArg) -> Result<Vec<FnArg>, syn::Error> {
     match arg {
         FnArg::Captured(arg) => to_c_type(arg),
-        x => vec![x.clone()],
+        x => Ok(vec![x.clone()]),
     }
 }
 
-fn to_c_type(arg: &ArgCaptured) -> Vec<FnArg> {
-    macro_rules! map_types {
-        [$($rust:path => $c:ty,)*] => {{
-            let mut map = std::collections::HashMap::<Type, Type>::new();
-            $(map.insert(
-                syn::parse2(quote!{ $rust }).unwrap(),
-                syn::parse2(quote!{ $c }).unwrap(),
-            );)*
-            map
-        }}
-    }
+fn to_c_type(arg: &ArgCaptured) -> Result<Vec<FnArg>, syn::Error> {
+    TYPE_MAPPING.with(|map| {
+        let ArgCaptured { ty, pat, .. } = arg.clone();
+        match map.get(&ty).cloned() {
+            Some(types) => match types.as_slice() {
+                [c_ty] => Ok(vec![ArgCaptured { ty: c_ty.clone(), ..arg.clone() }.into()]),
+                [c_ty, len] => {
+                    let name = if let syn::Pat::Ident(syn::PatIdent { ident, .. }) = pat {
+                        let mut name = ident.to_string();
+                        name.push_str("_len");
+                        syn::Ident::new(&name, ident.span())
+                    } else {
+                        return Err(syn::Error::new(
+                            pat.span(),
+                            "pattern as parameters not supported",
+                        ));
+                    };
+                    Ok(vec![
+                        ArgCaptured { ty: c_ty.clone(), ..arg.clone() }.into(),
+                        ArgCaptured {
+                            pat: syn::PatIdent {
+                                ident: name,
+                                by_ref: None,
+                                mutability: None,
+                                subpat: None,
+                            }
+                            .into(),
+                            ty: len.clone(),
+                            ..arg.clone()
+                        }
+                        .into(),
+                    ])
+                }
+                _ => unreachable!(),
+            },
+            None => Ok(vec![arg.clone().into()]),
+        }
+    })
+}
 
-    let map = map_types![
-        bool => ::std::os::raw::c_char,
-        u8 => ::std::os::raw::c_uchar,
-        i8 => ::std::os::raw::c_char,
-        i16 => ::std::os::raw::c_short,
-        u16 => ::std::os::raw::c_ushort,
-        i32 => ::std::os::raw::c_int,
-        u32 => ::std::os::raw::c_uint,
-        i64 => ::std::os::raw::c_long,
-        u64 => ::std::os::raw::c_ulong,
-        CStr => *const ::std::os::raw::c_char,
-        CString => *mut ::std::os::raw::c_char,
+macro_rules! map_types {
+    [$($rust:ty => [$($c:ty),*],)*] => {{
+        let mut map = std::collections::HashMap::<Type, Vec<Type>>::new();
+        $(map.insert(
+            syn::parse2(quote!{ $rust })
+                .expect(concat!("cannot parse", stringify!($rust), "as type")),
+            vec![$(
+                syn::parse2(quote!{ $c })
+                    .expect(concat!("cannot parse", stringify!($c), "as type")),
+            )*],
+        );)*
+        map
+    }}
+}
+
+thread_local! {
+    /// This is an example for using doc comment attributes
+    pub static TYPE_MAPPING: std::collections::HashMap<Type, Vec<Type>> = map_types![
+        bool => [::std::os::raw::c_char],
+        u8 => [::std::os::raw::c_uchar],
+        i8 => [::std::os::raw::c_char],
+        i16 => [::std::os::raw::c_short],
+        u16 => [::std::os::raw::c_ushort],
+        i32 => [::std::os::raw::c_int],
+        u32 => [::std::os::raw::c_uint],
+        i64 => [::std::os::raw::c_long],
+        u64 => [::std::os::raw::c_ulong],
+        &'a CStr => [*const ::std::os::raw::c_char],
+        CString => [*mut ::std::os::raw::c_char],
+        Arc<str> => [*const ::std::os::raw::c_char, ::libc::size_t],
     ];
-
-    let ArgCaptured { ref ty, .. } = arg;
-    let c_ty = map.get(&ty).cloned().unwrap_or_else(|| ty.clone());
-
-    vec![ArgCaptured { ty: c_ty, ..arg.clone() }.into()]
 }
 
 pub struct Params {}
--- cthulhu_calling/tests/examples.rs
+++ cthulhu_calling/tests/examples.rs
@@ -2,12 +2,16 @@ use cthulhu_calling::call_with;
 
 #[test]
 fn test_bool() {
-    let res = call_with(quote::quote! {}, quote::quote! {
-        fn some_shit(yes: bool) {}
-    }).unwrap();
+    let res = call_with(
+        quote::quote! {},
+        quote::quote! {
+            fn foo(yes: bool) {}
+        },
+    )
+    .unwrap();
     let expected = quote::quote! {
-        extern "C" fn some_shit(yes: ::std::os::raw::c_char) {
-            fn some_shit(yes: bool) {}
+        extern "C" fn foo(yes: ::std::os::raw::c_char) {
+            fn foo(yes: bool) {}
             unimplemented!()
         }
     };
@@ -16,12 +20,16 @@ fn test_bool() {
 
 #[test]
 fn test_u32() {
-    let res = call_with(quote::quote! {}, quote::quote! {
-        fn some_shit(num: u32) {}
-    }).unwrap();
+    let res = call_with(
+        quote::quote! {},
+        quote::quote! {
+            fn foo(num: u32) {}
+        },
+    )
+    .unwrap();
     let expected = quote::quote! {
-        extern "C" fn some_shit(num: ::std::os::raw::c_uint) {
-            fn some_shit(num: u32) {}
+        extern "C" fn foo(num: ::std::os::raw::c_uint) {
+            fn foo(num: u32) {}
             unimplemented!()
         }
     };
@@ -30,14 +38,17 @@ fn test_u32() {
 
 #[test]
 fn cstr() {
-    let res = call_with(quote::quote! {}, quote::quote! {
-        fn some_shit<'a>(string_maybe: &'a CStr) {}
-    }).unwrap();
+    let res = call_with(
+        quote::quote! {},
+        quote::quote! {
+            fn foo<'a>(input: &'a CStr) {}
+        },
+    )
+    .unwrap();
     let expected = quote::quote! {
-        extern "C" fn some_shit(le: *const ::std::os::raw::c_char, len: ::libc::size_t) {
-            fn some_shit<'a>(string_maybe: &'a CStr) {}
-            let (le, args) = oh_god(..);
-            some_shit(le, args)
+        extern "C" fn foo(input: *const ::std::os::raw::c_char) {
+            fn foo<'a>(input: &'a CStr) {}
+            unimplemented!()
         }
     };
     assert_eq!(res.to_string(), expected.to_string());
@@ -45,14 +56,17 @@ fn cstr() {
 
 #[test]
 fn arc_str() {
-    let res = call_with(quote::quote! {}, quote::quote! {
-        fn some_shit(string_maybe: Arc<str>) {}
-    }).unwrap();
+    let res = call_with(
+        quote::quote! {},
+        quote::quote! {
+            fn foo(input: Arc<str>) {}
+        },
+    )
+    .unwrap();
     let expected = quote::quote! {
-        extern "C" fn some_shit(le: *const ::std::os::raw::c_char, len: ::libc::size_t) {
-            fn some_shit(string_maybe: Arc<str>) {}
-            let (le, args) = oh_god(...);
-            some_shit(le, args)
+        extern "C" fn foo(input: *const ::std::os::raw::c_char, input_len: ::libc::size_t) {
+            fn foo(input: Arc<str>) {}
+            unimplemented!()
         }
     };
     assert_eq!(res.to_string(), expected.to_string());
