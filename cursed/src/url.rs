dc1030b5150f349c6ebfe0e6d83138c3edb14974
Brendan Molloy <brendan@bbqsrc.net>
2020-03-16 13:07:28 +0100

    Fix URL implementation

--- cursed/src/url.rs
+++ cursed/src/url.rs
@@ -1,7 +1,4 @@
 use std::error::Error;
-use std::ffi::CString;
-
-use libc::c_char;
 use url::Url;
 
 use super::{FromForeign, InputType, ReturnType, Slice, ToForeign};
@@ -9,46 +6,46 @@ use super::{FromForeign, InputType, ReturnType, Slice, ToForeign};
 pub struct UrlMarshaler;
 
 impl InputType for UrlMarshaler {
-    type Foreign = *const c_char;
+    type Foreign = Slice<u8>;
 }
 
 impl ReturnType for UrlMarshaler {
-    type Foreign = *const c_char;
+    type Foreign = Slice<u8>;
 
     #[inline(always)]
     fn foreign_default() -> Self::Foreign {
-        std::ptr::null()
+        Default::default()
     }
 }
 
 // Url -> char pointer
-impl ToForeign<Url, *const c_char> for UrlMarshaler {
-    type Error = Box<dyn Error>;
+impl ToForeign<Url, Slice<u8>> for UrlMarshaler {
+    type Error = std::convert::Infallible;
 
     #[inline(always)]
-    fn to_foreign(url: Url) -> Result<*const c_char, Self::Error> {
-        let c_str = std::ffi::CString::new(url.as_str())?;
-        Ok(CString::into_raw(c_str).cast())
+    fn to_foreign(url: Url) -> Result<Slice<u8>, Self::Error> {
+        let url = url.to_string();
+        crate::StringMarshaler::to_foreign(url)
     }
 }
 
 // Result<Url> -> char pointer
-impl ToForeign<Result<Url, Box<dyn Error>>, *const c_char> for UrlMarshaler {
+impl ToForeign<Result<Url, Box<dyn Error>>, Slice<u8>> for UrlMarshaler {
     type Error = Box<dyn Error>;
 
     #[inline(always)]
-    fn to_foreign(result: Result<Url, Box<dyn Error>>) -> Result<*const c_char, Self::Error> {
-        result.and_then(UrlMarshaler::to_foreign)
+    fn to_foreign(result: Result<Url, Box<dyn Error>>) -> Result<Slice<u8>, Self::Error> {
+        result.and_then(|url| Ok(UrlMarshaler::to_foreign(url).unwrap()))
     }
 }
 
 // Option<Url> -> char pointer
-impl ToForeign<Option<Url>, *const c_char> for UrlMarshaler {
+impl ToForeign<Option<Url>, Slice<u8>> for UrlMarshaler {
     type Error = Box<dyn Error>;
 
     #[inline(always)]
-    fn to_foreign(option: Option<Url>) -> Result<*const c_char, Self::Error> {
-        option.map_or_else(|| Ok(std::ptr::null()), UrlMarshaler::to_foreign)
+    fn to_foreign(option: Option<Url>) -> Result<Slice<u8>, Self::Error> {
+        option.map_or_else(|| Ok(Default::default()), |url| Ok(UrlMarshaler::to_foreign(url).unwrap()))
     }
 }
 
@@ -58,7 +55,7 @@ impl<'a> FromForeign<Slice<u8>, Url> for UrlMarshaler {
 
     #[inline(always)]
     unsafe fn from_foreign(key: Slice<u8>) -> Result<Url, Self::Error> {
-        let s = crate::StrMarshaler::from_foreign(key)?;
-        Url::parse(s).map_err(|e| Box::new(e) as _)
+        let s = crate::StringMarshaler::from_foreign(key)?;
+        Url::parse(&s).map_err(|e| Box::new(e) as _)
     }
 }
